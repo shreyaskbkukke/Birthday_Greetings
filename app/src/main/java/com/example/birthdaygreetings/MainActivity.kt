7ba3abf2e29c4fc3e5ba5858f1cb8ded0abe70aa
Shreyas KB <shreyaskb333@gmail.com>
2023-11-24 11:03:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/birthdaygreetings/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.birthdaygreetings
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.birthdaygreetings.ui.theme.BirthdayGreetingsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayGreetingsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText( message: String, from: String, modifier: Modifier = Modifier ) {
+    Row{
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
+    }
+}
+
+@Preview(
+        showBackground = true
+        //,showSystemUi = true
+    ) @Composable
+fun GreetingPreview() {
+    BirthdayGreetingsTheme {
+        GreetingText( message = "Happy Birthday Sam!", from = "From Emma")
+    }
+}
