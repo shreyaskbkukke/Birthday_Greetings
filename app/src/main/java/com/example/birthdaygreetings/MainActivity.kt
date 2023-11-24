0b221f0950bd82e313b658d3b5a9b26ee9334f08
Shreyas KB <shreyaskb333@gmail.com>
2023-11-24 14:04:33 +0530

    UI altering

--- app/src/main/java/com/example/birthdaygreetings/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreetings/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.birthdaygreetings
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthdaygreetings.ui.theme.BirthdayGreetingsTheme
 
@@ -33,15 +38,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText( message: String, from: String, modifier: Modifier = Modifier ) {
-    Column {
+    Column (
+        verticalArrangement = Arrangement.Center,
+                modifier = modifier
+            .padding(8.dp)
+    ) {
         Text(
             text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
             fontSize = 36.sp,
+            modifier =  modifier
+                .padding(16.dp)
+                .align(Alignment.End)
         )
     }
 }
